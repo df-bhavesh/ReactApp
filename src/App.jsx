fbd5bdfb2d651966ac6a62fead6a30de673d96af
BHAVESH KRISHAN GARG <168732086+df-bhavesh@users.noreply.github.com>
2025-10-17 12:20:35 +0530

    Color Picker

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
+import ColorPicker from './Components/ColorPicker';
 function App() {
-
-
   return (
-    <>
-    </>
+    <div>
+      <ColorPicker />
+    </div>
   );
 }
 export default App;
--- src/Components/ColorPicker.jsx
+++ src/Components/ColorPicker.jsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import { useState } from 'react';
 
 const ColorPicker = () => {
-    return (
-        <div>
-
-        </div>
+    const [color, setColor] = useState("#111111");
+    const [copyColor, setCopyColor] = useState(false);
+    function handleColorChange(e) {
+        setColor(e.target.value);
+        setCopyColor(false)
+    }
+    const handleCopyColor = async () => {
+        try {
+            await navigator.clipboard.writeText(color);
+            setCopyColor(true);
+            setTimeout(() => { setCopyColor(false); }, 1500);
+        } catch (err) {
+            console.error('Failed to copy: ', err);
+        }
+    }
+return (
+        <div className='color-picker-container'>
+            <h1>Color Picker</h1>
+            <div className='color-display' style={{ backgroundColor: color }}>
+                <p> Selected Color : {color}</p>
+            </div>
+            <label htmlFor="colorPicker">Select Color</label>
+            <input type="color" name="" id="colorPicker" value={color} onChange={handleColorChange} />
+            <button className='copy-color-button' onClick={handleCopyColor}>
+                Copy Color
+            </button>
+            {copyColor && <p className='copy-clipboard'>✅ Copied to clipboard!</p>}
+        </div >
     );
 }
 
